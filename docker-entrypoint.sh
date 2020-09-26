fd6fcac8c16f054ada1283de80bd72021f497a3a
Adam Dunstan <adam@acnodal.io>
2020-09-26 15:52:26 -0400

    update config and implement cm watcher so router reconfigures on cm change

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 set -e
 
 sed -i "s/K8SIPADDR/${BIRD_HOST}/g" /usr/local/include/birdvars.conf
---
+++ wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Start Bird & config watcher
+
+set -m
+
+bird -fR &
+
+./reconfig.sh &
+
+
+fg %1
+
+
+
+
